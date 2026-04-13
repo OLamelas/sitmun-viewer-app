69e720d66dfa324ff9b8efc9a489d4c11706b5d0
mplanas <mplanas@nexusgeographics.com>
2026-04-13 16:09:07 +0200

    added url query task scope

--- src/app/services/more-info.service.ts
+++ src/app/services/more-info.service.ts
@@ -81,7 +81,7 @@ export class MoreInfoService {
     if (task?.scope === 'API') {
       return this.executeApiQuery(parameters, task, featureData);
     }
-    if (task?.scope === 'URL') {
+    if (task?.scope === 'URL' || task?.scope === 'url-query') {
       return this.executeUrlQuery(task, featureData);
     }
 
