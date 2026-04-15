e16a09b35ec488bac6bebb999cb1688c6322eaf4
olamelas <olamelas@nexusgeographics.com>
2026-04-15 15:55:22 +0200

    feat(viewer): Restore Router in auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { AuthenticationInterceptor } from '@auth/services/authentication.interceptor';
 import { AuthenticationService } from '@auth/services/authentication.service';
@@ -11,7 +12,7 @@ import { AuthenticationService } from '@auth/services/authentication.service';
       provide: HTTP_INTERCEPTORS,
       useClass: AuthenticationInterceptor,
       multi: true,
-      deps: [AuthenticationService]
+      deps: [AuthenticationService, Router]
     }
   ]
 })
